a041b42bf71f57cd291de1b3a3b9d1aaca0363ff
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-04-04 14:34:21 +0530

    Complete the header and nav responsiveness

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../header/header.css'
 import Logo from "../../assets/logo.png"
+import bars from "../../assets/bars.png"
 
 const Header = () => {
+    const mobileView = window.innerWidth <= 768 ? true : false;
+    const [menuOpened, setMenuOpened] = useState(false)
     return (
         <div className="header">
             <img src={Logo} alt="LogoImage" />
-            <ul className="header-menu">
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why Us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
 
+            {menuOpened === false && mobileView === true ? (
+                <div onClick={()=>setMenuOpened(true)} style={{ backgroundColor: 'var(--appColor)', padding: '0.5rem', borderRadius: '5px' }}>
+                    <img src={bars} alt="" style={{ width: "1.5rem", height: '1.5rem', marginTop: '15px' }} />
+                </div>
+            ) : (<ul className="header-menu">
+                <li onClick={()=>setMenuOpened(false)}>Home</li>
+                <li onClick={()=>setMenuOpened(false)}>Programs</li>
+                <li onClick={()=>setMenuOpened(false)}>Why Us</li>
+                <li onClick={()=>setMenuOpened(false)}>Plans</li>
+                <li onClick={()=>setMenuOpened(false)}>Testimonials</li>
+            </ul>)}
         </div>
     )
 }
